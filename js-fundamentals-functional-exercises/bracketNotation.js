64fdc6ebbc627264e4c85b6a086a1a270f48c9e3
sandraenciso <88852214+sandraenciso@users.noreply.github.com>
2021-08-24 08:12:18 -0500

    Updating comments bracketNotation.js

--- js-fundamentals-functional-exercises/bracketNotation.js
+++ js-fundamentals-functional-exercises/bracketNotation.js
@@ -1,6 +1,11 @@
-/******** PROPERTY ACCES ***********/
+/******** OBJECTS ***********/
 
-/*Bracket Notation*/
+///// Bracket Notation
+
+/*
+Bracket notation can also be used to create properties. Functionally, bracket notation is the same as dot notation.
+However, the syntax looks entirely different. Both notations are interchangeable.
+*/
 
 var person = [];
 
@@ -10,9 +15,10 @@ var who = person.name;
 
 person[0] = "I was not in the Billiards room";
 
-//person[plea] = "I would never!"; //ReferenceError: plea is not defined
+person[plea] = "I would never!"; //ReferenceError: plea is not defined
 
+//New Property "wouldShe"
 var plea = "wouldShe";
 person[plea] = "I would never!"; //The access is using a String. This is a prop of array
 
-//console.log(person) //[ 'I was not in the Billiards room', name: 'Mrs. White', wouldShe: 'I would never!' ]
+console.log(person) //[ 'I was not in the Billiards room', name: 'Mrs. White', wouldShe: 'I would never!' ]
