e8807909c5ce7624db7190ce15e6d9207de459bf
sandraenciso <88852214+sandraenciso@users.noreply.github.com>
2021-08-24 18:46:50 -0500

    Updating comments closureDemonstration2.js

--- js-fundamentals-functional-exercises/closureDemonstration2.js
+++ js-fundamentals-functional-exercises/closureDemonstration2.js
@@ -1,7 +1,11 @@
 /********* Advanced Scope: CLosure ********/
 
-/****** Closure Advanced Scope ******/
+/////Closure Advanced Scope
 
+/*
+In the calls to the function expression, we are accessing to the variable 'n' and this changes every time we call the function
+This is Closure
+*/
 const countClues = () => {
   let n = 0;
 
@@ -13,7 +17,7 @@ const countClues = () => {
 
 const c = countClues(), d = countClues();
 
-c.count(); //0
+c.count(); //0 Returns 0 because n++ returns the old value of n (its 0)
 d.count(); //0
 c.reset(); //0
 c.count(); //0
