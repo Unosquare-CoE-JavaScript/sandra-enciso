e2eeed5e5bc3871e72353a7e44ee753b1dd762d9
sandraenciso <88852214+sandraenciso@users.noreply.github.com>
2021-08-24 18:23:47 -0500

    Updating comments es5RewriteSolution.js

--- js-fundamentals-functional-exercises/es5RewriteSolution.js
+++ js-fundamentals-functional-exercises/es5RewriteSolution.js
@@ -1,17 +1,18 @@
 /******** Functions in Depth***********/
 
-/*Default parameters*/
+/////Default parameters
 
-const add = function(a , b = 2) {
+/*Examples using default parameters*/
+const add = function(a , b = 2) { //Default parameter: b = 2. If we don't send b, the default value will be 2
    console.log(this.arguments); //logs [3]
    return a + b; 
 };
-add(3); //5??
+add(3); //5
 
 //Write the function in ES5
 const add = function(a , b) {
-   b = b || 2;
+   b = b || 2; //If b doesn't contains any value, assign 2 as value for b
    console.log(this.arguments); //logs [3]
    return a + b; 
 };
-add(3); //5??
+add(3); //5
