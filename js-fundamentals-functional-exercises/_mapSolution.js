ccdd60c4d9062f1e26f5331a99656826c9cc45c3
sandraenciso <88852214+sandraenciso@users.noreply.github.com>
2021-08-24 17:15:49 -0500

    Updating comments _mapSolution.js

--- js-fundamentals-functional-exercises/_mapSolution.js
+++ js-fundamentals-functional-exercises/_mapSolution.js
@@ -1,10 +1,15 @@
 /******** .map() Function ***********/
 
-/*_.map Excercise*/
+/////_.map Excercise
 
-const _ = {};
+/*
+map() calls a provided callbackFn function once for each element in an array, in order, and constructs a new array from the results. 
+*/
 
-_.each = function(list, callback) {//this executes the callback for each element of the list
+const _ = {}; //Declares '_' object
+
+//Assigning a new property 'each'
+_.each = function(list, callback) {//this executes the callback for each element of the list (array or object)
   if(Array.isArray(list)){
     //loop through array
     for(let i = 0; i < list.length; i++){
@@ -21,6 +26,7 @@ _.each = function(list, callback) {//this executes the callback for each element
   //celebrate
 } 
 
+//Assigning a new property 'map' in which returns a new array created using the callback for each element of the list
 _.map = function(list, callback) { //map returns an array
   //create an empty array to store
   var storage = [];
@@ -32,22 +38,24 @@ _.map = function(list, callback) { //map returns an array
   return storage;
 }
 
-_.map([1,2,3], (val) => val+1)
-
+_.map([1,2,3], (val) => val+1) //Invoking map to return a new array that contains [n+1,...,n+1] where n is each element of the array
 
 
 const weapons = ['candlestick', 'lead pipe', 'revolver'];
 
+//Function expression which returns a string
 const makeBroken = function(item){
   return 'broken '+item;
 }
 
-const brokenWeapons = _.map(weapons, makeBroken);
+const brokenWeapons = _.map(weapons, makeBroken); //Creating a new array using map (map returns an array).
+//The parameters are the weapons array and the function expression makeBroken();
 
 brokenWeapons;
 
 _.map(weapons, makeBroken)
 
+//This function returns an object
 function CreateSuspectObjects(name) {
   return {
     name: name,
@@ -59,10 +67,10 @@ function CreateSuspectObjects(name) {
 
 var suspects = ['Miss Scarlet', 'Coronel Mustard'];
 
-var suspectsList = _.map(suspects, function(name){
+var suspectsList = _.map(suspects, function(name){ //Creating a new array using map (map returns an array).
   return CreateSuspectObjects(name);
 })
 
-_.each(suspects, function(suspect) {
-  console.log(suspect.speak);
+_.each(suspects, function(suspect) {//Each method executes a function for each element of an array
+  console.log(suspect.speak); //My name is 'name' (for each name of the list)
 })
