cf650545ebeca5257db0d444ebf5eef87cbfb208
sandraenciso <88852214+sandraenciso@users.noreply.github.com>
2021-08-24 18:20:42 -0500

    Updating comments defaultParameters.js

--- js-fundamentals-functional-exercises/defaultParameters.js
+++ js-fundamentals-functional-exercises/defaultParameters.js
@@ -1,9 +1,9 @@
 /******** Functions in Depth***********/
 
-/*Default parameters*/
+/////Default parameters
 
-const add = function(a , b = 2) {
+const add = function(a , b = 2) { ///We can use default parameters values, in this case b = 2
    console.log(this.arguments); //logs [3]
    return a + b; 
 };
-add(3); //5??
+add(3); //5... How this call sends only one parameter, the default parameter is 2
