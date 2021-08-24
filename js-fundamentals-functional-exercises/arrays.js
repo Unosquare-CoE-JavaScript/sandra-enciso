fa025179cfed6210809962390a99a888f74f192d
sandraenciso <88852214+sandraenciso@users.noreply.github.com>
2021-08-24 07:58:59 -0500

    Updating comments arrays.js

--- js-fundamentals-functional-exercises/arrays.js
+++ js-fundamentals-functional-exercises/arrays.js
@@ -1,7 +1,16 @@
-/******** PROPERTY ACCES ***********/
+/******** OBJECTS ***********/
 
-/*Arrays*/
-var person = [];
+///// ARRAYS
+
+/*
+Arrays are data structure, they are commonly used for ordered data like lists.
+Arrays are list-like objects whose prototype has methods to perform traversal and mutation operations.
+Arrays cannot use strings as element indexes, but must use integers.
+Setting or accessing via non-integers using bracket notation (or dot notation) will not set or retrieve an element from the array list itself, 
+but will set or access a variable associated with that array's object property collection.
+*/
+
+var person = []; //Declares an array
 person.name = "Mrs. White"; //this would be a property of the array
 
 var who = person.name; // "Mrs. White"
