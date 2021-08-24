4d85635846fd2378ae6467e7ed4dd777f3b2dc17
sandraenciso <88852214+sandraenciso@users.noreply.github.com>
2021-08-24 17:45:14 -0500

    Updating comments filterApplicationSolution.js

--- js-fundamentals-functional-exercises/filterApplicationSolution.js
+++ js-fundamentals-functional-exercises/filterApplicationSolution.js
@@ -1,11 +1,17 @@
 /******** .filter() Function ***********/
 
-/*.filter() Excercise*/
+//filter() Excercise
+
+/*
+filter() calls a provided callbackFn function once for each element in an array, 
+and constructs a new array of all the values for which callbackFn returns a value that coerces to true
+*/
 
 const _ = {};
 
-_.each = function(list, callback) {
-  if(Array.isArray(list)){
+//Creating a new property 'each' which executes a callback function for each element of an array or an object
+_.each = function(list, callback) { //Parameters array, callback function.
+  if(Array.isArray(list)){ 
     for(let i = 0; i < list.length; i++){
       callback(list[i], i, list);
     }
@@ -16,13 +22,14 @@ _.each = function(list, callback) {
   }
 }
 
+//Filter works similar to map, but filter returns a new array filtering 
 _.filter = (array, cb) => {
   const storage = [];
-  _.each(array, (item, i, list) => {
+  _.each(array, (item, i, list) => { //loops an array using the .each method
     if(cb(item, i, array)){
       storage.push(item);
     }
-  })
+  })// Using 'each' instead 'for' structure
   /*for(let i = 0; i < array.length; i++){
     if(cb(array[i], i, array)){
       storage.push(array[i])
@@ -31,7 +38,7 @@ _.filter = (array, cb) => {
   return storage;
 }
 
-
+//Object which contains the data of the clue characters
 const videoData = [
     {
         name: 'Miss Scarlet',
@@ -107,6 +114,7 @@ const videoData = [
     }
 ];
 
+//Array which contains the characters which are present (true)
 _.filter(videoData, function(suspectObject){
   return suspectObject.present
 })
