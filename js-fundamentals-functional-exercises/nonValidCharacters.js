8fa67e3650bb8226475aba52dcbb05737019f7d8
sandraenciso <88852214+sandraenciso@users.noreply.github.com>
2021-08-24 08:22:27 -0500

    Updating comments nonValidCharacters.js

--- js-fundamentals-functional-exercises/nonValidCharacters.js
+++ js-fundamentals-functional-exercises/nonValidCharacters.js
@@ -1,22 +1,28 @@
-/******** PROPERTY ACCES ***********/
+/******** OBJECTS ***********/
 
-/*Non-valid Characters*/
+/////Non-valid Characters
+
+/*
+You can access to properties of an object using bracket notation, but you would use strings to assing any property.
+It's weird to use characters like numbers or special characters.
+*/
 
 var box = {};
 
-box['material'] = "cardboard";
+box['material'] = "cardboard"; //Good name of property
 
 box[0] = 'meow'; //You can assing property 0 to an object, but this is weird
 
 box['&*'] = "testing 123"; // This isn't a good name, its weird too
 
-var text = box['&*'];
+var text = box['&*']; //Assingning using a weird name property
 
-/*console.log(box); 
-{
+console.log(box); //'testing 123'
+
+/*{
   '0': 'meow',
   material: 'cardboard',
   '&*': 'testing 123'
 }*/
 
-//console.log(text);//'testing 123'
+console.log(text); //'testing 123'
