d06b5bf8e6a559a3578b1fac2a1a2499bfc4809d
sandraenciso <88852214+sandraenciso@users.noreply.github.com>
2021-08-24 18:13:07 -0500

    Updating comments arrayForm.js

--- js-fundamentals-functional-exercises/arrayForm.js
+++ js-fundamentals-functional-exercises/arrayForm.js
@@ -1,9 +1,13 @@
 /******** Functions in Depth***********/
 
-/*Array-Like Object*/
+/////Array-Like Object
+
+/*
+ES6 Array.from() method creates a new array instance from an array-like object
+*/
 
 const constructArr = function() {
-  const arr = Array.from(arguments);
+  const arr = Array.from(arguments); //All the arguments are converted to an array
   arr.push('the billiards room?');
   return arr.join(' ');
 };
