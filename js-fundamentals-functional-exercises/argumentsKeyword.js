bd0524f64d4cb39301d4b850973b44f7170b947e
sandraenciso <88852214+sandraenciso@users.noreply.github.com>
2021-08-24 17:37:53 -0500

    Updating comments argumentsKeyword.js

--- js-fundamentals-functional-exercises/argumentsKeyword.js
+++ js-fundamentals-functional-exercises/argumentsKeyword.js
@@ -1,9 +1,10 @@
 /******** .filter() Function ***********/
 
-/*.filter() Excercise*/
+///// .filter() Excercise
 
 const _ = {};
 
+//Creating a new property 'each' which executes a callback function for each element of an array or an object
 _.each = function(list, callback) {
   if(Array.isArray(list)){
     for(let i = 0; i < list.length; i++){
@@ -16,6 +17,7 @@ _.each = function(list, callback) {
   }
 }
 
+//Creating a new property 'map' which returns a new array, this array is created executing a callback function for each element of an array
 _.map = function(list, callback) { //map returns an array
   //create an empty array to store
   var storage = [];
@@ -27,22 +29,27 @@ _.map = function(list, callback) { //map returns an array
   return storage;
 }
 
-_.filter = (array, cb) => {
+/*
+filter() calls a provided callbackFn function once for each element in an array, 
+and constructs a new array of all the values for which callbackFn returns a value that coerces to true
+*/
+//Filter works similar to map, but filter returns a new array filtering 
+_.filter = (array, cb) => { //Parameters array, callback function.
   const storage = [];
-  _.each(array, (item, i, list) => {
+  _.each(array, (item, i, list) => { //loops an array using the .each method
     if(cb(item, i, array)){
       storage.push(item);
     }
-  })
+  })// Using 'each' instead 'for' structure
   /*for(let i = 0; i < array.length; i++){
     if(cb(array[i], i, array)){
       storage.push(array[i])
     }
   }*/
-  return storage;
+  return storage; //returns a new array
 }
 
-
+//Object which contains the data of the clue characters
 const videoData = [
     {
         name: 'Miss Scarlet',
@@ -118,25 +125,30 @@ const videoData = [
     }
 ];
 
-const suspects = _.filter(videoData, function(suspectObject){
+//Array which contains the characters which are present (true)
+const suspects = _.filter(videoData, function(suspectObject){ ///Using filter to filtering the data
   return suspectObject.present
 });
-
+//Array which contains the names of the characters which are present (true)
 const suspectsNames = _.map( suspects, suspect => {
   return suspect.name
 })
-
+//We can use 'arguments' to access to the parameters that we send
 const createTuple = (a, b, c, d) => {
   //console.log(arguments); // [] the arguments that are passed 
-  return [[a, c],[ b, d]];
+  return [[a, c],[ b, d]]; //Desesctructuring the arguments
 }
 
 createTuple('It', 'be', 'could', 'anyone', 'no one'); 
-// => ??
+// => Returns 'It could be anyone'
 
+/*
+Spread syntax can be used when all elements from an object or array need to be included in a list of some kind.
+When we invoke the function, we pass it all the values in the array using the spread syntax (...) and the array name
+*/
 //SPREAD
 const createTuple2 = (a, b, c, ...d) => {
   return [[a, c],[ b, d]];
 }
 
-createTuple2('It', 'be', 'could', 'anyone', 'no one'); 
+createTuple2('It', 'be', 'could', 'anyone', 'no one'); // => Returns 'It could be anyone'
