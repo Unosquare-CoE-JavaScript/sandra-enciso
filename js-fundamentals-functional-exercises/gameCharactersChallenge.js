e80282d3435c4bb28dd19a5421b3392807459b75
sandraenciso <88852214+sandraenciso@users.noreply.github.com>
2021-08-24 08:33:54 -0500

    Updating comments gameCharactersChallenge.js

--- js-fundamentals-functional-exercises/gameCharactersChallenge.js
+++ js-fundamentals-functional-exercises/gameCharactersChallenge.js
@@ -1,6 +1,6 @@
-/******** PROPERTY ACCES ***********/
+/******** OBJECTS ***********/
 
-/*Game Characters Challenge*/
+/////Game Characters Challenge
 /* Exercise
 Create an object using bracket and bot notation that represents the characters and related data you may find in a game of Clue.
 */
@@ -8,11 +8,13 @@ Create an object using bracket and bot notation that represents the characters a
 var game = {};
 
 game.muerderer = "??";
+//Remember, you can assing a property using bracket notation
+//Assigning an array of objects to  property 'weapons'
 game['weapons'] = [
   {type: 'lasers', location: 'lab'}, 
   {type: 'angry cats'}, 
   'dish soap'
 ];
 
-game.name[0] = 'Miss Scarlet';
-game.name.push('Mr. Green')
+game.name[0] = 'Miss Scarlet'; //Assingning a value to an array of property name
+game.name.push('Mr. Green'); //Using property(array method) push, you can assign a value for the last element of an array 
