b508b7082e961d5f62fcd8f1ec4ecdc421f0a1b5
sandraenciso <88852214+sandraenciso@users.noreply.github.com>
2021-08-24 09:33:11 -0500

    Updating comments forEachFunction.js

--- js-fundamentals-functional-exercises/forEachFunction.js
+++ js-fundamentals-functional-exercises/forEachFunction.js
@@ -1,7 +1,12 @@
 /******** .forEach() Function ***********/
 
-/*Using Functions*/
+/////Using Functions
 
+/*
+Converting data into a Class
+*/
+
+//This function return an object
 function CreateSuspectObjects(name) {
   return {
     name: name,
@@ -12,10 +17,15 @@ function CreateSuspectObjects(name) {
   }
 }
 
+//Array of names
 var suspects = ['Miss Scarlet', 'Coronel Mustard', 'Mr. White'];
 
-var suspectsList = [];
+var suspectsList = []; //Empty array
 
-for(let i = 0; i < suspects.length; i++){
+/*
+Looping the 'susptects' array to make 'push' to 'sustectList' using the function 'CreateSuspectObjects' which returns an object, 
+using the elements of 'suspects' array
+*/
+for(let i = 0; i < suspects.length; i++){ 
   suspectsList.push(CreateSuspectObjects(suspects[i]));
 }
