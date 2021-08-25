54eced4c0ea825c8c7893ea436d550ab0f66a3dc
sandraenciso <88852214+sandraenciso@users.noreply.github.com>
2021-08-24 20:16:26 -0500

    Updating comments translateIntoES6.js

--- js-fundamentals-functional-exercises/translateIntoES6.js
+++ js-fundamentals-functional-exercises/translateIntoES6.js
@@ -1,6 +1,10 @@
 /********* HIGHER-ORDER FUNCTIONS AND CALLBACKS ********/
 
-/****** TRANSLATE INTO ES6 ******/
+/////TRANSLATE INTO ES6
+/*
+doMathSoIDontHaveTo is a higher order function, is assigned to a variable, use curry and returns a function
+In this example, the code is modified to ES6
+*/
 
 var increment = function(n){ return n + 1; };
 
@@ -8,11 +12,13 @@ var square = function(n){ return n*n; };
 
 var doMathSoIDontHaveTo = function(n, func){ return func(n); };
 
-doMathSoIDontHaveTo(5, square);
+doMathSoIDontHaveTo(5, square); //This send a function as parameter and returns a function
 
 doMathSoIDontHaveTo(4, increment);
 
 ///////////////// ES6 BELOW
+
+/* This works as the same as the code above, but this is ES6 syntax*/
 
 const increment2 = n => n+1;
 const square2 = n => n*n;
