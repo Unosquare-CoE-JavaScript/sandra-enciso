21a24fc0ae6d7883eec48b4e2fb1e5011bd65c8e
sandraenciso <88852214+sandraenciso@users.noreply.github.com>
2021-08-24 19:05:45 -0500

    Updating comments creatingClosure.js

--- js-fundamentals-functional-exercises/creatingClosure.js
+++ js-fundamentals-functional-exercises/creatingClosure.js
@@ -1,7 +1,17 @@
 /********* Advanced Scope: CLosure ********/
 
-/****** Closure Advanced Scope ******/
+/////Closure Advanced Scope
 
+/*
+Closure gives you access to an outer function’s scope from an inner function.
+In JavaScript, closures are created every time a function is created, at function creation time.
+*/
+
+/*
+The example below demonstrates the closure.
+The function alerter inside myAlert, modifies 'count' variable increment by 1 its value
+and then, log the result to concat x variable to count variable
+*/
 const myAlert = () => {
     const x = "Help! I think I found a clue!";
     let count = 0;
@@ -13,7 +23,7 @@ const myAlert = () => {
   
   const funcAlert = myAlert(); 
   const funcAlert2 = myAlert(); 
-  funcAlert(); //1
-  funcAlert(); //2
-  funcAlert2(); //1
+  funcAlert(); //Help! I think I found a clue! 1 ... How ++count, ++ is before the variable, this returns the new value of the variable
+  funcAlert(); //Help! I think I found a clue! 2
+  funcAlert2(); //Help! I think I found a clue! 1
   
